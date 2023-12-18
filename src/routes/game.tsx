3728ba9e1d370db1d1fda897ef5e09636498897d
nat730 <natanaelcolart@gmail.com>
2023-12-18 16:59:11 +0100

    test install

--- src/routes/game.tsx
+++ src/routes/game.tsx
@@ -19,6 +19,10 @@ const Game = () => {
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout | undefined>(undefined);
   const navigate = useNavigate();
   const [playClickSound] = useSound(clickSound);
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+
+
 
   useEffect(() => {
     if (divsClicked === 0) {
@@ -62,13 +66,47 @@ const Game = () => {
     }
   }, [divsClicked, timeElapsed, intervalId, navigate]);
 
-  const handlePauseClick = () => {
-    clearInterval(intervalId);
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout | undefined;
+
+    const handleInterval = () => {
+      setStartTime((prevStartTime) => {
+        const elapsedSeconds = (Date.now() - prevStartTime) / 1000;
+        setTimeElapsed(Number(elapsedSeconds.toFixed(3)));
+        return prevStartTime;
+      });
+    };
+    
+    if (!isPaused) {
+      intervalId = setInterval(handleInterval, 100);
+    }
+
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [isPaused]); 
+
+  const handlePauseResumeClick = () => {
+    setIsPaused(!isPaused);
   };
 
+
   const handleFullscreenClick = () => {
-      document.documentElement.requestFullscreen();
+    const element = document.documentElement;
+
+    if (!document.fullscreenElement) {
+      element.requestFullscreen().then(() => setIsFullscreen(true));
+    }
   };
+
+  const handleExitFullscreenClick = () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen().then(() => setIsFullscreen(false));
+    }
+  };
+
 
   const handleShareClick = () => {
     if (navigator.share) {
@@ -82,14 +120,56 @@ const Game = () => {
 
   const handleInstallClick = () => {
     if (window.matchMedia("(display-mode: standalone)").matches) {
-      console.log("Le jeu est déjà installé.");
+      console.log("Le jeu est déjà installé en mode standalone.");
       //@ts-ignore
     } else if (window.navigator.standalone) {
       console.log("Le jeu est déjà installé sur iOS.");
     } else {
-      console.log("Installer le jeu...");
+      installApp();
     }
   };
+
+  const installApp = () => {
+      if (confirm("Installer le jeu?")) {
+      addToHomeScreen();
+    }
+  };
+  
+
+  const addToHomeScreen = () => {
+    const link = document.createElement("link");
+    link.rel = "apple-touch-icon";
+    link.href = "/path/to/icon.png";
+    document.head.appendChild(link);
+      const manifest = {
+      short_name: "Nom Court",
+      name: "Nom Complet",
+      icons: [
+        {
+          src: "/assets/icons",
+          sizes: "192x192",
+          type: "image/png",
+        },
+      ],
+      start_url: "/",
+      display: "standalone",
+      background_color: "#ffffff",
+      theme_color: "#000000",
+    };
+  
+    const jsonManifest = JSON.stringify(manifest);
+    const blob = new Blob([jsonManifest], { type: "application/json" });
+    const manifestFile = URL.createObjectURL(blob);
+  
+    const manifestLink = document.createElement("link");
+    manifestLink.rel = "manifest";
+    manifestLink.href = manifestFile;
+    document.head.appendChild(manifestLink);
+  
+    console.log("Le jeu a été ajouté à l'écran d'accueil.");
+  };
+  
+  
 
   const handleDivClick = () => {
     setDivsClicked(divsClicked + 1);
@@ -105,6 +185,8 @@ const Game = () => {
     setGameOver(false);
   };
 
+
+
   return (
     <div className="game-container">
       <h1>Jeu</h1>
@@ -112,10 +194,16 @@ const Game = () => {
         <p>Divs cliquées: {divsClicked}</p>
         <p>Chrono: {timeElapsed} sec</p>
       </div>
-      <button onClick={handlePauseClick}>Pause</button>
-      <button onClick={handleFullscreenClick}>Fullscreen</button>
       <button onClick={handleShareClick}>Share</button>
       <button onClick={handleInstallClick}>Install</button>
+      <button onClick={handlePauseResumeClick}>
+        {isPaused ? "Resume" : "Pause"}
+      </button>
+      <button onClick={isFullscreen ? handleExitFullscreenClick : handleFullscreenClick}>
+        {isFullscreen ? "Remove Fullscreen" : "Fullscreen"}
+      </button>
+      
+
 
       {!gameOver ? (
         <img

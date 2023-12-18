47d12f2700d867015a29b1152a419de16d5c5809
nat730 <natanaelcolart@gmail.com>
2023-12-18 16:14:23 +0100

    ajout son+image

--- src/routes/game.tsx
+++ src/routes/game.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import "../Game.css";
 import { useNavigate } from "react-router-dom";
+// @ts-ignore
+import { useSound } from "use-sound";
+import clickSound from '/home/nat/dev/kill_the_div/public/sound/Pig.mp3';
 
 const getRandomPosition = () => ({
   top: Math.random() * 95 + "%",
@@ -14,16 +17,22 @@ const Game = () => {
   const [gameOver, setGameOver] = useState(false);
   const [position, setPosition] = useState(getRandomPosition());
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout | undefined>(undefined);
+  const navigate = useNavigate();
+  const [playClickSound] = useSound(clickSound);
 
   useEffect(() => {
     if (divsClicked === 0) {
       setStartTime(Date.now());
-  
+
+      if ("vibrate" in navigator) {
+        navigator.vibrate(200);
+      }
+
       setIntervalId(setInterval(() => {
         setStartTime((prevStartTime) => {
           const elapsedSeconds = (Date.now() - prevStartTime) / 1000;
           setTimeElapsed(Number(elapsedSeconds.toFixed(3)));
-          return prevStartTime;   
+          return prevStartTime;
         });
       }, 100));
     }
@@ -33,16 +42,61 @@ const Game = () => {
     if (divsClicked === 10) {
       setGameOver(true);
       clearInterval(intervalId);
+
+      if ("Notification" in window) {
+        Notification.requestPermission().then((permission) => {
+          if (permission === "granted") {
+            new Notification("Jeu terminé", {
+              body: `Temps écoulé: ${timeElapsed} sec`,
+            });
+          }
+        });
+      }
+
+      const bestScore = localStorage.getItem("bestScore");
+      if (!bestScore || parseFloat(bestScore) < timeElapsed) {
+        localStorage.setItem("bestScore", timeElapsed.toString());
+      }
+
       navigate("/endgame/" + timeElapsed);
     }
-  }, [divsClicked]);
+  }, [divsClicked, timeElapsed, intervalId, navigate]);
+
+  const handlePauseClick = () => {
+    clearInterval(intervalId);
+  };
+
+  const handleFullscreenClick = () => {
+      document.documentElement.requestFullscreen();
+  };
+
+  const handleShareClick = () => {
+    if (navigator.share) {
+      navigator.share({
+        title: "Jouez à ce super jeu!",
+        text: "Venez battre mon meilleur score!",
+        url: window.location.href,
+      });
+    }
+  };
+
+  const handleInstallClick = () => {
+    if (window.matchMedia("(display-mode: standalone)").matches) {
+      console.log("Le jeu est déjà installé.");
+      //@ts-ignore
+    } else if (window.navigator.standalone) {
+      console.log("Le jeu est déjà installé sur iOS.");
+    } else {
+      console.log("Installer le jeu...");
+    }
+  };
 
   const handleDivClick = () => {
     setDivsClicked(divsClicked + 1);
     setPosition(getRandomPosition());
+    playClickSound();
+    navigator.vibrate(200);
   };
-
-  const navigate = useNavigate();
 
   const handleReplayClick = () => {
     setDivsClicked(0);
@@ -58,17 +112,25 @@ const Game = () => {
         <p>Divs cliquées: {divsClicked}</p>
         <p>Chrono: {timeElapsed} sec</p>
       </div>
+      <button onClick={handlePauseClick}>Pause</button>
+      <button onClick={handleFullscreenClick}>Fullscreen</button>
+      <button onClick={handleShareClick}>Share</button>
+      <button onClick={handleInstallClick}>Install</button>
+
       {!gameOver ? (
-        <div
+        <img
           id="clickable-div"
+          src="/image_game/test.jpg"
+          alt="Clickable Image"
           onClick={handleDivClick}
           className="clickable-div"
           style={position}
-        ></div>
+        />
       ) : (
         <div>
           <h2>Partie terminée</h2>
           <p>Temps écoulé: {timeElapsed} sec</p>
+          <p>Meilleur score: {localStorage.getItem("bestScore")} sec</p>
           <button onClick={handleReplayClick}>Rejouer</button>
         </div>
       )}
